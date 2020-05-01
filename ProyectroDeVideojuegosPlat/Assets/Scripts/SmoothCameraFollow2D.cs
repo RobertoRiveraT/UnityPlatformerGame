f0e9fe5b0c0353ba4cd65f937314e313656bc273
ed9ar <a01339939@itesm.mx>
2020-05-01 15:15:38 -0500

    Moving plats
    Plats

---
+++ ProyectroDeVideojuegosPlat/Assets/Scripts/SmoothCameraFollow2D.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class SmoothCameraFollow2D : MonoBehaviour {
+
+	public float dampTime = 0.15f;
+	private Vector3 velocity = Vector3.zero;
+	public Transform target;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (target) {
+            // Obtiene la posición que tiene el objeto que estamos siguiendo
+            // según la vista de la cámara
+			Vector3 point = GetComponent<Camera>().WorldToViewportPoint(new Vector3(target.position.x, (target.position.y + 2.2f), target.position.z));
+			// la diferencia que existe entre la posición del objeto y la posición que tiene la cámara
+			// respecto al objeto
+			Vector3 delta = new Vector3(target.position.x, (target.position.y + 2.2f), target.position.z) - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+			// establecemos el destino es decir, la posición que tengo como cámara más la posición
+			// delta obtenida anteriormente
+			Vector3 destination = transform.position + delta;
+			// Movemos la cámara utilizando el método de Smooth Damp
+			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+		}
+	}
+}
---
+++ ProyectroDeVideojuegosPlat/Assets/Scripts/SoundManagerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerController : MonoBehaviour
+{
+    public static AudioClip playerWalk, jumpSound, hookSound;
+    static AudioSource audioSrc;
+    public static bool playing = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerWalk = Resources.Load<AudioClip>("pickup");
+        jumpSound = Resources.Load<AudioClip>("playerJump");
+        hookSound = Resources.Load<AudioClip>("hook");
+
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void PlaySound(string clip){
+        switch (clip)
+        {
+            case "pickup":
+                audioSrc.PlayOneShot(playerWalk);
+                playing = true;
+                break;
+            case "playerJump":
+                audioSrc.PlayOneShot(jumpSound);
+                break;
+            case "hook":
+                audioSrc.PlayOneShot(hookSound);
+                break;
+        }
+    }
+}
