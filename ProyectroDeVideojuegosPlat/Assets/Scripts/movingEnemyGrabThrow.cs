5b729aad6f1d312ad54c38290deda31327ee9f9e
[Ignacio] <54185068+IgnacioAlvarado@users.noreply.github.com>
2020-05-02 13:41:23 -0500

    Moving Enemies fix
    Moving Enemies fix

---
+++ ProyectroDeVideojuegosPlat/Assets/Scripts/movingEnemyGrabThrow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingEnemyGrabThrow : MonoBehaviour
+{
+	public GameObject one;
+	public GameObject two;
+	public GameObject papa;
+	
+    // Update is called once per frame
+    void Update()
+    {
+    	if(this.gameObject.tag=="grab"){
+    		Destroy(one);
+    		Destroy(two);
+    	}
+    }
+    
+    private void OnCollisionEnter2D(Collision2D other){
+    	Debug.Log("col");
+    	if(this.gameObject.tag=="thrownRight"||this.gameObject.tag=="thrownLeft"||this.gameObject.tag=="thrown"){
+    		Destroy(papa);
+    		
+    	}
+	}
+}
