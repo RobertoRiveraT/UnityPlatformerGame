6d08953997cb2756eb1a2fb0d989fe868f2e9fc0
[Ignacio] <54185068+IgnacioAlvarado@users.noreply.github.com>
2020-05-04 21:55:05 -0500

    New Enemy type
    Shooting Enemy

---
+++ ProyectroDeVideojuegosPlat/Assets/Scripts/EnemyProj.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProj : MonoBehaviour
+{
+    public float vel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Translate(new Vector3(vel,0,0));
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other) {
+    	GameObject ney= other.gameObject;
+    	if(ney.tag!="checkpoint"){
+    		Destroy(gameObject);
+    		}
+    }
+    private void OnCollisionEnter2D(Collision2D other) {
+    	Destroy(gameObject);
+    		
+    }
+}
---
+++ ProyectroDeVideojuegosPlat/Assets/Scripts/ShootEnemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootEnemy : MonoBehaviour
+{
+    public Transform shooter;
+	public GameObject mil;
+	public float shootTime;
+	
+	// Use this for initialization
+	void Start () {
+		
+		StartCoroutine(shootGood());
+	}
+	
+	private void shoot(){
+		Instantiate (mil, shooter.position,Quaternion.identity);
+	}
+	
+	
+	
+	IEnumerator shootGood(){
+		while(true){
+			if(this.gameObject.tag=="bad"){
+			yield return new WaitForSeconds(shootTime);
+			shoot();
+			}else{
+				yield break;
+			}
+		}
+	}
+}
